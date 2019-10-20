245f0a670a2e30bac6188e6d41b37bc606ab312a
Ilona Zhu <ilonazhu@ilonas-mbp.home>
2019-10-20 23:38:08 +0100

    Added files for the website

---
+++ functions.js
@@ -0,0 +1,23 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "#9ba2a3";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#f2fdff";
+}
+
+function openNav2() {
+  document.getElementById("sidenav").style.width = "300px";
+  document.getElementById("AboutHer").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "#9ba2a3";
+}
+
+function closeNav2() {
+  document.getElementById("sidenav").style.width = "0";
+  document.getElementById("AboutHer").style.marginLeft = "0";
+  document.body.style.backgroundColor = "#f2fdff";
+}
